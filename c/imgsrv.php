5d9c5d3468d3c981e853644ab93086f5983499bb
don kretz <dakretz@gmail.com>
2015-09-02 17:40:52 -0700

    Applied proper file suffix function in place of string expression.

--- c/imgsrv.php
+++ c/imgsrv.php
@@ -7,10 +7,10 @@ $pagename  = ArgPageName();
 
 $pg = new DpPage($projectid, $pagename);
 $imgpath = $pg->ImageFilePath();
+$sfx = FileNameExtension($imgpath);
+//$sfx = right($imgpath, 3);
 
-$sfx = right($imgpath, 3);
-
-header("Content-length: " .filesize($imgpath));
+header("Content-length: " . filesize($imgpath));
 header("Content-type: image/$sfx");
 $fp = fopen($imgpath, "rb");
 fpassthru($fp);
