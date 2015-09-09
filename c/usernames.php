2c78db8061c1f050e5235ee0a312c7528553f459
don kretz <dakretz@gmail.com>
2015-09-09 13:44:24 -0700

    User list utility

--- c/usernames.php
+++ c/usernames.php
@@ -1,11 +1,12 @@
-<?PHP
+<?php
 ini_set('display_errors', 1);
 error_reporting(E_ALL);
 
 $relPath = "../c/pinc/";
 include_once $relPath . 'dpinit.php';
 
-echo_head("DPC Query");
+echo html_head("DPC Query");
+echo "<h1 class='center'>DPC Query</h1>\n";
 
 $sql = "SELECT username, real_name from users
         ORDER BY username";
@@ -18,3 +19,5 @@ $tbl->AddColumn("<Real name", "real_name");
 $tbl->SetRows($rows);
 
 $tbl->EchoTable();
+echo html_end();
+?>
