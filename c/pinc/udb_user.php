91dabedb34e5ccee25e97fa4f544e6c75f119e47
dkretz <dakretz@gmail.com>
2015-09-12 16:13:51 -0700

    Delete udb_user.php

--- c/pinc/udb_user.php
+++
@@ -1,8 +0,0 @@
-<?PHP
-
-$db_server   = 'localhost';
-$db_user     = 'usrDPCanada';
-$db_password = 'CanadaWin10';
-$db_name     = 'dbDPCanada';
-
-$site_managers = array("dkretz", "ionav", "simple simon", "De2164");
