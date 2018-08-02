486221300d334a16c6de47ec516d872433dc48ad
Alex White <alexwhite@alumni.uwaterloo.ca>
2018-08-02 19:56:42 -0400

    Add search of the Clearance Spreadsheet to extended search

--- c/search2.php
+++ c/search2.php
@@ -14,12 +14,15 @@ require_once $relPath."dpinit.php";
 
 $Context = new DpContext();
 
+$googleKey = "1lU5VeCLMIPhkqqGc-g0iE7Ld5EU5b-sI7DcoKiUwZPo";
+$googleURL = "https://spreadsheets.google.com/feeds/list/$googleKey/od6/public/full?alt=json";
 $qtitle         = Arg("qtitle");
 $qauthor        = Arg("qauthor");
 $qpm            = Arg("qpm");
 $qpp            = Arg("qpp");
 $qppv           = Arg("qppv");
 $qfadedpage     = Arg("qfadedpage");
+$qclearance     = Arg("qclearance");
 $qlang          = ArgArray("qlang");
 $qgenre         = ArgArray("qgenre");
 $qphase         = ArgArray("qphase");
@@ -220,6 +223,10 @@ if ($qfadedpage == "on")
     $fp = "checked";
 else
     $fp = "";
+if ($qclearance == "on")
+    $clearance = "checked";
+else
+    $clearance = "";
 echo "
 <form id='searchform' name='searchform' method='POST'>
 <div id='divsearch' class='center' onClick='eSetSort(event)'>
@@ -244,7 +251,14 @@ echo "
 				<div class='w20 left lfloat'></div>
                 <div class='lfloat w75'>
                 <input id='qfadedpage' name='qfadedpage' type='checkbox' $fp>
-                Search title and author on Fadedpage.com as well as local projects.
+                Search title and author on Fadedpage.com.
+                </div>
+			</div>
+			<div>
+				<div class='w20 left lfloat'></div>
+                <div class='lfloat w75'>
+                <input id='qclearance' name='qclearance' type='checkbox' $clearance>
+                Search title and author in the Clearance Spreadsheet.
                 </div>
 			</div>
 			<!--
@@ -411,8 +425,7 @@ if ($qfadedpage == "on" && ($qtitle != "" || $qauthor != "")) {
             echo " Only $nbooks have been returned, refine your search.";
         echo "</p>";
 
-        $tbl->SetRowCount(count($rows));
-        //$tbl->SetPaging($pagenum, $rowsperpage);
+        $tbl->SetRowCount($nbooks);
         $tbl->SetRows($rows);
 
         echo "<div class='center' onclick='eSetSort(event)'>\n";
@@ -437,6 +450,98 @@ if ($qfadedpage == "on" && ($qtitle != "" || $qauthor != "")) {
             ";
     }
 }
+if ($qclearance == "on" && ($qtitle != "" || $qauthor != "")) {
+
+    // See https://developers.google.com/sheets/api/v3/worksheets
+    $curl = curl_init();
+    curl_setopt($curl, CURLOPT_URL, $googleURL);
+    curl_setopt($curl, CURLOPT_TIMEOUT, 300);
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $result = curl_exec($curl);
+    curl_close($curl);
+    $result = json_decode($result, $assoc = TRUE);
+    //echo html_comment(print_r($result, TRUE));
+    $feed = $result['feed'];
+    $rows = $feed['entry'];
+    $n = count($rows);
+    //echo "<pre>";
+    //echo print_r($result, TRUE);
+    //print_r($n);
+    //print_r($rows[$n-1]);
+    //echo "</pre>";
+
+    //echo html_comment(print_r($rows[0], TRUE));
+    //echo html_comment(print_r($rows[5], TRUE));
+    // row[0] = Last Updated...
+    // row[1] = _cre1l: CP/PM, _chk2m: Year of, _ciyn3: DPC Clearance, _clrrx: Posting Number
+    // row[2] = _cokwr: Contributor First Name, _cpzh4: Title, _cre1l: requestor, _chk2m: Public'n, _ciyn3: Code #, _clrrx: at FP
+    // Data starts on row[3], remove the first three rows.
+    unset($rows[0]);
+    unset($rows[0]);
+    unset($rows[0]);
+
+    $title_cell = 'gsx$_cpzh4';
+    $last_cell = 'gsx$_cn6ca';
+    $first_cell = 'gsx$_cokwr';
+    $fpid_cell = 'gsx$_clrrx';
+    $published_cell = 'gsx$_chk2m';
+    $clearance_cell = 'gsx$_ciyn3';
+    $pm_cell = 'gsx$_cre1l';
+
+    $r = array();
+    foreach ($rows as $row) {
+        $title = getCell($row, $title_cell);
+        $author = getCell($row, $last_cell);
+        $first = getCell($row, $first_cell);
+        if ($first != '')
+            $author .= ', ' . $first;
+        if (
+            ($qtitle == ''  || stripos($title, $qtitle) === FALSE)
+        &&  ($qauthor == '' || stripos($author, $qauthor) === FALSE)
+        )
+            // Does not match criteria
+            continue;
+
+        $fpid = getCell($row, $fpid_cell);
+        $published = getCell($row, $published_cell);
+        $clearance = getCell($row, $clearance_cell);
+        $pm = getCell($row, $pm_cell);
+        $r[] = array(
+            'id'=>$fpid,
+            'title'=>$title,
+            'author'=>$author,
+            'published'=>$published,
+            'clearance'=>$clearance,
+            'pm'=>$pm
+        );
+    }
+    $rows = $r;
+
+    $tbl = new DpTable("tblsearch", "dptable sortable w95");
+    $tbl->AddColumn("<FP ID", 'id');
+    $tbl->AddColumn("<".$titleCaption, 'title');
+    $tbl->AddColumn("<".$authorCaption, 'author');
+    $tbl->AddColumn("<Published", 'published');
+    $tbl->AddColumn("<Clearance", 'clearance');
+    $tbl->AddColumn("<PM", 'pm');
+    $tbl->SetRowCount(count($rows));
+
+	$nbooks = count($rows);
+    if ($nbooks == 0)
+        echo _("<p class='bold'>No books in the Clearance Spreadsheet matched the search criteria.</p>");
+    else {
+
+        echo _("<p class='hpadded'>$nbooks books in the Clearance Spreadsheet matched the search criteria.");
+        echo "</p>";
+
+        $tbl->SetRowCount($nbooks);
+        $tbl->SetRows($rows);
+
+        echo "<div class='center' onclick='eSetSort(event)'>\n";
+        $tbl->EchoTable();
+        echo "</div>";
+    }
+}
 
 
 echo "
@@ -444,6 +549,13 @@ echo "
 theme("", "footer");
 exit;
 
+function getCell($row, $cell)
+{
+    if (isset($row[$cell]))
+        return trim($row[$cell]['$t']);
+    else
+        return '';
+}
 
 function is_available($row) {
 	return preg_match("/_avail/", $row['phase'])
