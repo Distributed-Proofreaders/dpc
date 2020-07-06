733d114e892e02c4f4172abf0807445d2459b539
Alex White <alexwhite@alumni.uwaterloo.ca>
2020-07-05 20:00:15 -0400

    Merge tables; add pages/users

--- c/activity_hub.php
+++ c/activity_hub.php
@@ -76,57 +76,99 @@ echo "
     . _("Want to help out the site by providing material for us to proofread? ")
     . "Check out the $cp_link forum and leave a message that you want to help.</li>\n";
 
+echo "<li>Page-based Round Processing<br>\n";
+
+echo _("
+    <table class='bordered hub_table'>
+    <tr class='navbar'><td>Round</td>
+                       <td>On Hold</td>
+                       <td>Available</td>
+                       <td>Total<br>Projects</td>
+                       <td>Today's<br>Pages<br>Processed</td>
+                       <td>Today's<br>Active<br>Users</td>
+    </tr>");
 /** @var Round $round */
 foreach ( $Context->Rounds() as $round ) {
-	$roundid = $round->RoundId();
+    $roundid = $round->RoundId();
     $phase_icon_path = "$dyn_dir/stage_icons/$roundid.jpg";
     $phase_icon_url  = "$dyn_url/stage_icons/$roundid.jpg";
     if ( file_exists($phase_icon_path) ) {
         $round_img = "<img src='$phase_icon_url' alt='($roundid)' align='middle'>";
-    }
-    else {
+    } else {
         $round_img = "($roundid)";
     }
-	$rname = RoundIdName($roundid);
+    $rname = RoundIdName($roundid);
     $rdesc = RoundIdDescription($roundid);
     $rlink = link_to_round($roundid, $rname);
 
     echo "
-        <li> 
-        <hr class='w75'>
-        $round_img $rlink <br> $rdesc <br /><br />\n";
+        <tr>
+        <td>$round_img $rlink <br> $rdesc</td>\n";
 
     summarize_projects($roundid);
+    echo "</tr>\n";
 }
+
+echo "</table>\n";
+echo "</li><li>Book Creation<br>\n";
+echo "<table class='bordered hub_table'>";
 
 $phase = "PP";
 $rname = NameForPhase($phase);
 $rdesc = DescriptionForPhase($phase);
 $rlink = link_to_pp($rname);
-echo "
-        <li>
-        <hr class='w75'>
-        ($phase) $rlink <br> $rdesc <br /><br />\n";
-summarize_projects( $phase );
 
-$n_checked_out = $dpdb->SqlOneValue("
+$u = $User->Username();
+$args = [ &$u ];
+$my_checked_out = $dpdb->SqlOneValuePS("
         SELECT COUNT(1) FROM projects
-        WHERE postproofer='{$User->Username()}'
-            AND phase='PP'");
-if ($n_checked_out) {
-    echo sprintf( _("You currently have %d projects checked out for Post Processing."), $n_checked_out );
-    echo "<br>\n";
+        WHERE postproofer=?
+            AND phase='PP'", $args);
+$row = $dpdb->SqlOneRow("
+     SELECT SUM(IFNULL(postproofer, '') = '') navail,
+            COUNT(1) ntotal,
+            SUM(smoothread_deadline > UNIX_TIMESTAMP() ) nsmooth
+     FROM projects where phase = 'PP'");
+$navail = $row["navail"];
+$ntotal = $row["ntotal"];
+$nchecked_out = $ntotal - $navail;
+$nsmooth = $row["nsmooth"];
+$msg = "";
+if ($my_checked_out) {
+    $msg = "You currently have $my_checked_out projects checked out for Post Processing.";
 }
+echo "
+    <tr>
+        <td></td>
+        <td class='navbar'>Available<br>for $phase</td>
+        <td class='navbar'>Checked<br>Out</td>
+        <td class='navbar'>Total<br>Projects</td>
+    </tr>
+    <tr>
+        <td>($phase) $rlink <br> $rdesc <br> $msg</td>
+        <td>$navail</td><td>$nchecked_out</td><td>$ntotal</td>
+    </tr>
+";
 
 
 $phase = "SR";
 $rname = _("Smooth Reading");
 $rdesc = _("Nearly completed projects are often made available for reading and checkproofing before posting.");
 $rlink = link_to_smooth_reading($rname);
+$nsmooth = $dpdb->SqlOneValue("
+     SELECT SUM(smoothread_deadline > UNIX_TIMESTAMP() ) nsmooth
+     FROM projects where phase = 'PP'");
 echo "
-        <li>
-        <hr class='w75'>
-        ($phase) $rlink <br> $rdesc <br /><br />\n";
+    <tr>
+        <td rowspan='2'>($phase) $rlink <br> $rdesc</td>
+        <td class='navbar'></td>
+        <td class='navbar'>Available for<br>Smooth Reading</td>
+        <td class='navbar'></td>
+    </tr>
+    <tr>
+        <td></td><td>$nsmooth</td><td></td>
+    </tr>
+";
 
 
 // ----------------------------------------------------
@@ -135,19 +177,39 @@ $phase = "PPV";
 $rname = NameForPhase($phase);
 $rdesc = DescriptionForPhase($phase);
 $rlink = link_to_ppv($rname);
-echo "
-        <hr class='w75'>
-        <li> ($phase) $rlink <br> $rdesc <br /><br />\n";
-summarize_projects( $phase );
-
-$n_checked_out = $dpdb->SqlOneValue("
+$row = $dpdb->SqlOneRow("
+     SELECT SUM(CASE WHEN IFNULL(ppverifier, '') = '' THEN 1 ELSE 0 END) navail,
+            COUNT(1) ntotal
+     FROM projects where phase = 'PPV'");
+$navail = $row["navail"];
+$ntotal = $row["ntotal"];
+// If there are none in PPV, then we get an empty row.
+if (empty($navail))
+    $navail = 0;
+if (empty($ntotal))
+    $ntotal = 0;
+$nchecked_out = $ntotal - $navail;
+$n_checked_out = $dpdb->SqlOneValuePS("
         SELECT COUNT(1) FROM projects
-        WHERE ppverifier='{$User->Username()}'
-            AND phase='PPV'");
+        WHERE ppverifier=?
+            AND phase='PPV'", $args);
+$msg = "";
 if ($n_checked_out) {
-    echo sprintf( _("You currently have %d projects checked out for PP Verification."), $n_checked_out );
-    echo "<br>\n";
+    $msg = "You currently have $n_checked_out projects checked out for PP Verification.";
 }
+echo "
+    <tr>
+        <td rowspan='2'>($phase) $rlink <br> $rdesc <br> $msg</td>
+        <td class='navbar'>Available<br>for $phase</td>
+        <td class='navbar'>Checked<br>Out</td>
+        <td class='navbar'>Total<br>Projects</td>
+    </tr>
+    <tr>
+        <td>$navail</td><td>$nchecked_out</td><td>$ntotal</td>
+    </tr>
+";
+
+echo "</table>\n";
 
 echo "
     </li>
@@ -159,74 +221,21 @@ theme("", "footer");
 function summarize_projects( $phase) {
     global $dpdb;
 
-    if($phase == "P1" || $phase == "P2" || $phase == "P3" || $phase == "F1" || $phase == "F2") {
-        $row = $dpdb->SqlOneRow("
-            SELECT SUM(CASE WHEN NOT EXISTS (SELECT 1 FROM project_holds
-                                             WHERE projectid = p.projectid
-                                                 AND phase = '$phase')
-                            THEN 1 ELSE 0
-                       END) navail,
-                   COUNT(1) ntotal
-            FROM projects p WHERE p.phase = '$phase'");
-        $navail = $row["navail"];
-        $ntotal   = $row["ntotal"];
-        $nwaiting = $ntotal - $navail;
+    $row = $dpdb->SqlOneRow("
+        SELECT SUM(CASE WHEN NOT EXISTS (SELECT 1 FROM project_holds
+                                         WHERE projectid = p.projectid
+                                             AND phase = '$phase')
+                        THEN 1 ELSE 0
+                   END) navail,
+               COUNT(1) ntotal
+        FROM projects p WHERE p.phase = '$phase'");
+    $navail = $row["navail"];
+    $ntotal   = $row["ntotal"];
+    $nwaiting = $ntotal - $navail;
+    $today = PhaseCountToday($phase);
+    $users = PhaseUsersActiveToday($phase);
 
-        echo _("
-            <table class='bordered hub_table'>
-            <tr class='navbar'><td rowspan='2'>All projects</td>
-                               <td>On Hold</td>
-                               <td>Available</td>
-                               <td>Total<br>Projects</td></tr>
-
-            <tr><td>$nwaiting</td><td>$navail</td><td>$ntotal</td></tr>
-            </table>\n");
-        return;
-    }
-
-
-    if($phase == "PP") {
-        $row = $dpdb->SqlOneRow("
-             SELECT SUM(IFNULL(postproofer, '') = '') navail,
-                    COUNT(1) ntotal,
-                    SUM(smoothread_deadline > UNIX_TIMESTAMP() ) nsmooth
-             FROM projects where phase = '$phase'");
-        $navail = $row["navail"];
-        $ntotal = $row["ntotal"];
-        $nchecked_out = $ntotal - $navail;
-        $nsmooth = $row["nsmooth"];
-        echo _("
-        <table class='bordered hub_table'>
-        <tr class='navbar'><td rowspan='2'>All projects</td>
-                           <td>Available<br>for $phase</td>
-                           <td>Checked<br>Out</td>
-                           <td>Available for<br>Smooth Reading</td>
-                           <td>Total<br>Projects</td></tr>\n");
-        echo "
-        <tr><td>$navail</td><td>$nchecked_out</td><td>$nsmooth</td><td>$ntotal</td></tr>
-        </table>\n";
-    }
-    else {
-        $row = $dpdb->SqlOneRow("
-             SELECT SUM(CASE WHEN IFNULL(ppverifier, '') = '' THEN 1 ELSE 0 END) navail,
-                    COUNT(1) ntotal
-             FROM projects where phase = '$phase'");
-        $navail = $row["navail"];
-        $ntotal = $row["ntotal"];
-        // If there are none in PPV, then we get an empty row.
-        if (empty($navail))
-            $navail = 0;
-        if (empty($ntotal))
-            $ntotal = 0;
-        $nchecked_out = $ntotal - $navail;
-        echo _("
-        <table class='bordered hub_table'>
-        <tr class='navbar'><td rowspan='2'>All projects</td>
-                           <td>Available<br>for $phase</td>
-                           <td>Checked<br>Out</td>
-                           <td>Total<br>Projects</td></tr>\n");
-        echo "
-        <tr><td>$navail</td><td>$nchecked_out</td><td>$ntotal</td></tr>
-        </table>\n";
-    }
+    echo "<td>$nwaiting</td><td>$navail</td><td>$ntotal</td><td>$today</td><td>$users</td>";
 }
+
+// vim: sw=4 ts=4 expandtab
