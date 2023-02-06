fa0bafcc8bd3994b691e43074629b898ea5a9219
Alex White <alexwhite@alumni.uwaterloo.ca>
2023-02-06 15:15:22 -0500

    Don't use localhost

--- c/pinc/udb_user.php
+++ c/pinc/udb_user.php
@@ -1,8 +1,8 @@
 <?PHP
 
-$db_server   = 'localhost';
+$db_server   = '127.0.0.1';
 $db_user     = 'usrDPCanada';
 $db_password = 'CanadaWin10';
 $db_name     = 'dbDPCanada';
 
-$site_managers = array("ionav", "simple simon");
+$site_managers = array("ionav", "simple simon", "alexwhite", "DPCanada");
