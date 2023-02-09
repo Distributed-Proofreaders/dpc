c546609af72fd2e27546a26fb0beb3f45cb0761b
Alex White <alexwhite@alumni.uwaterloo.ca>
2023-02-09 17:04:18 -0500

    Move db user/pass to ini file

--- c/pinc/udb_user.php
+++ c/pinc/udb_user.php
@@ -1,8 +1,13 @@
-<?PHP
+<?php
 
-$db_server   = '127.0.0.1';
-$db_user     = 'usrDPCanada';
-$db_password = 'CanadaWin10';
-$db_name     = 'dbDPCanada';
+$db_server = ini_get("mysqli.default_host");
+$db_user = ini_get("mysqli.default_user");
+$db_password = ini_get("mysqli.default_pw");
+if (strpos($_SERVER['SERVER_NAME'], "sandbox") !== false)
+    $db_name     = 'sandbox_DPCanada';
+else
+    $db_name     = 'dbDPCanada';
 
-$site_managers = array("ionav", "simple simon", "alexwhite", "DPCanada");
+$site_managers = array("ionav", "alexwhite", "DPCanada");
+
+?>
